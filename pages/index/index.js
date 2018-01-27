5144d2a6b46fca8fee940a9e05dca7b463a6647b
dongxianbin <379229152@qq.com>
2018-01-27 17:38:06 +0800

    0127

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,68 @@
+// index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    imgUrls: [
-      '../image/swiper2.jpeg',
-      '../image/swiper3.jpeg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    circular: true,
-
-    videourl: "http://cdntf.infoxgame.com//recharge/donggexiao/adv.mp4",
-    advurl1: "../image/adv1.jpeg",
-    advtext1: "  黄金视力眼贴是一款用于缓解眼部疲劳的眼贴产品。黄金视力眼贴主要通过对眼部的贴敷，起到滋养睫状神经节，促进睫状神经节更好的发挥调节视力，恢复眼部健康的作用。黄金视力眼贴是由李时珍现代生物医药集团、郑州百消丹药业、吉林徐氏生物三大药厂联合生产，北京微海联合电子商务有限公司独家运营的眼贴品牌。黄金视力眼贴内含36味天然野生纯正中草药，秉承传统中医治眼精髓，使药物活性成分直接穿透眼部皮肤组织及血眼屏障，直达眼底，并通过溶解、雾化、拔浊、润眼四个阶段，高倍强效作用于眼部神经、血管、眼球及病变部位，融治疗、调理、抗菌消炎、滋养四位一体，黄金视力眼贴专业治疗各种眼病。",
-    advtext2: "  黄金视力眼贴内含36味天然野生纯正中草药，秉承传统中医治眼精髓，运用二氧化碳超低温萃取技术、LY3次精度回流技术、4小时精准缓释技术，27倍浓缩36味中药精华，组方宏大，药效强劲，并从中提取出分子团非常小、渗透力极强的AM活性因子；根据眼部的生理结构，采用德国先进的蛋白透皮技术，在冰片，薄荷脑起物理推动作用下，将独特的配方进行彻底的透皮吸收，使药物活性成分直接穿透眼部皮肤组织及血眼屏障，直达眼底。",
-    advtext3: "  36味植物萃取液作用于眼周的鱼腰穴、睛明穴、赞竹穴、丝竹空穴、童子瞭穴、承泣穴、太阳穴、四白穴、球后穴等相关穴位，经皮给药发挥治疗作用，使药物充分促进睫状肌和视神经等受损组织的弹性恢复，调节房水的分泌与流动，让晶状体和角膜得到充分的滋润与营养，不但能够放松视神经，而且还能放松脑部神经，更有效的改善血液循环，清除“眼部垃圾”，呵护眼部健康，从而达到治疗眼疾，缓解视疲劳的作用！",
-    advtext4: "  36味纯正中药通过由内而外的调和养，从根本上解决各类眼部问题。并通过溶解、雾化、拔浊、润眼四个阶段，高倍强效作用于眼部神经、血管、眼球及病变部位，融治疗、调理、抗菌消炎、滋养四位一体，专业治疗各种眼病。\n\n  黄金视力眼贴是36味纯正中草药的天然成分，无副作用，无防腐剂，无激素、无酒精，无刺激性气味！\n\n  “医用级 不解释”已成为其它眼贴类产品追求的制高点，正在被模仿，永远不会被超越！",
-    advtext5: "  黄金视力眼贴良好的品质深受广大消费者的追捧，更是众多医院眼科医生的推荐产品，由于电子产品和移动互联网的发展，用眼过度将会常态化，黄金视力眼贴的出现，适应了爱眼护眼的大趋势和市场需求！",
-    advtext6: "  黄金视力眼贴，专业保护您的眼睛。",
+    huanying: "",
+    tishi: "",
+    motto: '黄金视力眼贴-慧慧代理欢迎你',
+    motto1: '点击头像进入小程序',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
+ 
+  //事件处理函数
+  bindViewTap: function () {
+    wx.switchTab({
+      url: '../main/main',
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true,
+        huanying: this.data.motto,
+        tishi: this.data.motto1
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true,
+          huanying: this.data.motto,
+          tishi: this.data.motto1
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true,
+            huanying: this.data.motto,
+            tishi: this.data.motto1
+          })
+        }
+      })
+    }
+  
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true,
+      huanying: this.data.motto,
+      tishi: this.data.motto1
+    })
+    
+  }
 })
